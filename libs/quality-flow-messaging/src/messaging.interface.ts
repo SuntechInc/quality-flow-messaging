df32604ca8a07cf149f6f7c5610855bd99bdda70
lucas.previato <lucaspreviato@live.com>
2025-07-26 14:15:39 -0300

    Change event name

--- libs/quality-flow-messaging/src/messaging.interface.ts
+++ libs/quality-flow-messaging/src/messaging.interface.ts
@@ -4,7 +4,7 @@
 export const KAFKA_SERVICE_TOKEN = 'KAFKA_SERVICE';
 
 /** Payload básico para eventos de módulo de empresa */
-export interface TentantModuleEvent {
+export interface TenantCompanyModuleEvent {
   companyId:  string;
   moduleCode: string;
   segment:    string;
